ea97226327e15ba506732ae5b30727fbfe2a1d3c
Emmanuel Ritzler <142004500+Hamdo22@users.noreply.github.com>
2023-12-20 20:38:31 +0100

    Test for finding new base

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const returnRandBase = () => {
   const dnaBases = ['A', 'T', 'C', 'G'];
   return dnaBases[Math.floor(Math.random() * 4)];
 };
+console.log(returnRandBase());
 
 // Returns a random single stand of DNA containing 15 bases
 const mockUpStrand = () => {
@@ -12,11 +13,49 @@ const mockUpStrand = () => {
   }
   return newStrand;
 };
+console.log(mockUpStrand());
+
+const pAequorFactory = (specimenNum, dna) => {
+  const dnaBases = ['A', 'T', 'C', 'G'];
+  console.log(dnaBases);
+  return {
+    specimenNum: specimenNum,
+    dna: dna,
+    mutate() {
+      let originalBase = returnRandBase();
+      let indexToDelete = dnaBases.indexOf(i);
+      // remove the output of returnRandBase from the dnaBases array. Then assign the Math.floor and .random method with * 3 to the new array
+      console.log(modifiedBase);
+    }
+  }
+};
+
+console.log(pAequorFactory(1, mockUpStrand()));
 
 
+const dnaBases = ['A', 'T', 'C', 'G'];
+let slicedBase = dnaBases.slice(dnaBases[0], 4);
+console.log(slicedBase);
 
+let originalBase = returnRandBase();
+console.log(originalBase);
 
+let indexToDelete = dnaBases.indexOf(originalBase);
+console.log(indexToDelete);
 
+let modifiedBase = dnaBases.splice(indexToDelete, 1);
+console.log(dnaBases);
 
+modifiedBase = dnaBases[Math.floor(Math.random() * 3)];
+console.log(modifiedBase);
+
+const MockUpStrand = () => {
+  const newStrand = [];
+  for (let i = 0; i < 15; i++) {
+    newStrand.push(returnRandBase());
+  }
+  return newStrand;
+};
+console.log(MockUpStrand());
 
 
