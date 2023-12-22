09b693d98009020bc873aa1742e9005ed58ab41f
Emmanuel Ritzler <142004500+Hamdo22@users.noreply.github.com>
2023-12-22 17:20:29 +0100

    Add strand comparison in percentage

--- main.js
+++ main.js
@@ -17,45 +17,61 @@ console.log(mockUpStrand());
 
 const pAequorFactory = (specimenNum, dna) => {
   const dnaBases = ['A', 'T', 'C', 'G'];
-  console.log(dnaBases);
   return {
     specimenNum: specimenNum,
     dna: dna,
     mutate() {
+      const dnaBases = ['A', 'T', 'C', 'G'];
+      let slicedBase = dnaBases.slice(dnaBases[0], 4);
+      
       let originalBase = returnRandBase();
-      let indexToDelete = dnaBases.indexOf(i);
-      // remove the output of returnRandBase from the dnaBases array. Then assign the Math.floor and .random method with * 3 to the new array
+      
+      let indexToDelete = dnaBases.indexOf(originalBase);
+      
+      let modifiedBase = dnaBases.splice(indexToDelete, 1);
+      
+      modifiedBase = dnaBases[Math.floor(Math.random() * 3)];
       console.log(modifiedBase);
-    }
+      
+      const MockUpStrand = () => {
+        const newStrand = [];
+        for (let i = 0; i < 15; i++) {
+          newStrand.push(returnRandBase());
+        }
+        return newStrand;
+      };
+      const newStrand = MockUpStrand()
+      return newStrand;
+    },
+    compareDNA() {
+      let originalMock = mockUpStrand();
+      let modifiedMock = organism.mutate();
+      let identicalBases = [];
+
+      for(let i = 0; i < originalMock.length; i++) {
+        if(originalMock[i] === modifiedMock[i]) {
+          identicalBases.push(originalMock[i]);
+        }
+      };
+      console.log(originalMock);
+      console.log(modifiedMock);
+      console.log(originalMock[0]);
+      console.log(modifiedMock[0]);
+      console.log(originalMock.length);
+      console.log(modifiedMock.length);
+      console.log(identicalBases.length);
+      console.log(identicalBases);
+      let percentageOfSimilarity = (identicalBases.length / originalMock.length) * 100;
+      return `The strand #1 and the strand #2 have ${percentageOfSimilarity}% DNA in common.`;
+    } 
   }
 };
+const organism = pAequorFactory(1, mockUpStrand);
+console.log(organism.mutate());
+console.log(organism.compareDNA());
 
-console.log(pAequorFactory(1, mockUpStrand()));
 
 
-const dnaBases = ['A', 'T', 'C', 'G'];
-let slicedBase = dnaBases.slice(dnaBases[0], 4);
-console.log(slicedBase);
 
-let originalBase = returnRandBase();
-console.log(originalBase);
-
-let indexToDelete = dnaBases.indexOf(originalBase);
-console.log(indexToDelete);
-
-let modifiedBase = dnaBases.splice(indexToDelete, 1);
-console.log(dnaBases);
-
-modifiedBase = dnaBases[Math.floor(Math.random() * 3)];
-console.log(modifiedBase);
-
-const MockUpStrand = () => {
-  const newStrand = [];
-  for (let i = 0; i < 15; i++) {
-    newStrand.push(returnRandBase());
-  }
-  return newStrand;
-};
-console.log(MockUpStrand());
 
 
